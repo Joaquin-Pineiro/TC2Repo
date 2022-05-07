3f96c01bdf4b5cc90b90d45d86d409974afb9af7
Joaquin-Pineiro <103330691+Joaquin-Pineiro@users.noreply.github.com>
2022-05-07 18:01:44 -0300

    Agrego pretty print al python

--- Tareas Semanales/Tarea Semanal 2/Tarea Semanal 2.py
+++ Tareas Semanales/Tarea Semanal 2/Tarea Semanal 2.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy import signal as sig
-from splane import pzmap, bodePlot
+from splane import pzmap, bodePlot, pretty_print_bicuad_omegayq
 
 
 C = 10e-6
@@ -14,7 +14,7 @@ R3 = 100e3
 w0 = 1/(C*R3)
 
 
-num = np.array([0,0,-R3/R1  * w0**2])
+num = np.array([-R3/R1  * w0**2])
 den = np.array([1,1/(C*R2),w0**2])
 
 
@@ -24,8 +24,10 @@ T = sig.TransferFunction(num , den)
 
 plt.close('all')
 
+pretty_print_bicuad_omegayq(num, den, displaystr = True)
+
 bodePlot(T,[-2,2],[30,-10], 'none', 'none', 'Filtro Pasa Bajos', 1000 )
-pzmap(T,True, 'Filtro Pasa Todo', 'none', 'none')
+pzmap(T,True, 'Filtro Pasa Bajos', 'none', 'none')
 
 ##TRANSFERENCIA NORMALIZADA
 Omega_z=R3
@@ -37,10 +39,13 @@ C_n=C*Omega_z*Omega_w
 
 
 
-num_n = np.array([0,0,-1/R1_n])
+num_n = np.array([-1/R1_n])
 den_n = np.array([1,1/(R2_n),1])
 
 T_n = sig.TransferFunction(num_n , den_n)
+
+
+pretty_print_bicuad_omegayq(num_n, den_n, displaystr = True)
 
 
 bodePlot(T,[-2,2],[30,-10], 'none', 'none', 'Filtro Pasa Bajos', 1000 )
@@ -50,15 +55,14 @@ pzmap(T_n,True, 'Filtro Pasa Todo Normalizado', 'none', 'none')
 ##TRANSFERENCIA Butterworth con epsilon=1 (WB = W0)
 
 R2=70e3
-num_b = np.array([0,0,-R3/R1  * w0**2])
+num_b = np.array([-R3/R1  * w0**2])
 den_b = np.array([1,1/(C*R2),w0**2])
 
 
 
 T_b = sig.TransferFunction(num_b , den_b)
 
-
-
+pretty_print_bicuad_omegayq(num_b, den_b, displaystr = True)
 
 bodePlot(T_b,[-2,2],[30,-10], 'none', 'none', 'Filtro Pasa Bajos ButterWorth', 1000 )
 pzmap(T_b,True, 'Filtro Pasa Todo ButterWorth', 'none', 'none')
